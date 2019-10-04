0b71f5d69bced245b5736e6b046dd172bf73b9c6
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 22:15:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -114,8 +114,9 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = "GUI"   
-  programmer_hash[:alan_kay][:known_for]
+
 end
+  programmer_hash[:alan_kay][:known_for]
 
 def adding_to_dennis
 	programmer_hash = 
